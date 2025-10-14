fb1490ac5c01c6b158a88c7623935af2006c7189
afdanny <26141380+afdanny@users.noreply.github.com>
2025-10-14 17:08:40 +0200

    chore: add base package structure (controller, service, repository, model)
    🏗️ Added empty package structure to prepare next feature branches:
    - controller/: REST endpoints
    - service/: business logic layer
    - repository/: persistence layer (JPA)
    - model/: entities and enums

---
+++ src/main/java/ch/afdanny/technicalexercise/clientcontractapi/controller/ClientController.java
@@ -0,0 +1,9 @@
+package ch.afdanny.technicalexercise.clientcontractapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clients")
+public class ClientController {
+}
