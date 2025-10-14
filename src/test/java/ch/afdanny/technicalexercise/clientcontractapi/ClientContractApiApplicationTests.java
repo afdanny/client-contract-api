c73c87e3dde3ee1444aaddd980dcb21c99570e68
afdanny <26141380+afdanny@users.noreply.github.com>
2025-10-14 13:12:47 +0200

    feat: initialize Spring Boot 3.5.6 project for API Factory technical exercise
    - Created initial project structure with Spring Initializr
    - Set up Java 21 and Spring Boot 3.5.6
    - Included dependencies:
      - Spring Web
      - Spring Data JPA
      - PostgreSQL Driver
      - Liquibase Migration
      - Validation
      - Lombok
      - Testcontainers
    - Configured packaging as JAR
    - Project ready for Liquibase changelog and Docker setup

---
+++ src/test/java/ch/afdanny/technicalexercise/clientcontractapi/ClientContractApiApplicationTests.java
@@ -0,0 +1,15 @@
+package ch.afdanny.technicalexercise.clientcontractapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ClientContractApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/ch/afdanny/technicalexercise/clientcontractapi/TestClientContractApiApplication.java
@@ -0,0 +1,11 @@
+package ch.afdanny.technicalexercise.clientcontractapi;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestClientContractApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(ClientContractApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
